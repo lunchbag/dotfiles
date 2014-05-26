cce0ac62c7cf225aa2296b2af5152a1d09db1e43
NAME <EMAIL>
2014-05-26 02:26:19 -0700

    More stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
-	source ~/.bash_profile
+	source ~/.zshrc
 }
 doIt
 unset doIt
